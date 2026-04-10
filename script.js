ee2c94d4ed969085c710136bfc62ba9e2b093c79
DTTL <69401746+downtathelast@users.noreply.github.com>
2026-04-10 14:55:27 -0700

    Update form action URL in script.js

--- script.js
+++ script.js
@@ -155,7 +155,7 @@ document.getElementById("reviveForm").addEventListener("submit", function (e) {
 
   const form = document.createElement("form");
   form.method = "POST";
-  form.action = "https://script.google.com/macros/s/AKfycbyih8RHw5VreUay97BCc6B7F1IE3wKWVqXFHfysyOOBb6KP427oUIJoR1xCZ0IPoOs/exec";
+  form.action = "https://script.google.com/macros/s/AKfycbxf9j4r0keNHzZeAosiv4xxYDnzfqhuMQdEL4LH2E5Q_tVAne79DcPMcC5TR4Tj6c-1/exec";
   form.target = "hidden_iframe";
 
   const playerName = this.querySelector('[name="playerName"]').value;
