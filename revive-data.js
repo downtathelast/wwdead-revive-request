a0aea2bca77fe0c151ddfc2275c0aed26529106c
DTTL <69401746+downtathelast@users.noreply.github.com>
2026-04-11 16:14:35 -0700

    Change maintainer status to 'Not maintained'
    Updated maintainer status for various locations in REVIVE_DATA.

--- revive-data.js
+++ revive-data.js
@@ -1,8 +1,310 @@
 const REVIVE_DATA = {
+  "Barrville": [
+    { name: "Knill Road", maintainer: "Not maintained" }
+  ],
+  "Brooke Hills": [
+    { name: "St. Pius's Church", maintainer: "Not maintained" }
+  ],
+  "Brooksville": [
+    { name: "Cemetery 34,65", maintainer: "Not maintained" },
+    { name: "Cemetery 38,67", maintainer: "Not maintained" },
+    { name: "The Culling Building", maintainer: "Not maintained" },
+    { name: "Heal Walk", maintainer: "Not maintained" }
+  ],
+  "Buttonville": [
+    { name: "Cemetery 44,93", maintainer: "Not maintained" },
+    { name: "Cemetery 40,98", maintainer: "Not maintained" },
+    { name: "Wasteland 47,95", maintainer: "Not maintained" }
+  ],
+  "Chancelwood": [
+    { name: "Curton Mansion", maintainer: "Not maintained" },
+    { name: "The Harenc Building", maintainer: "Not maintained" },
+    { name: "Parrott Plaza", maintainer: "Not maintained" }
+  ],
+  "Crooketon": [
+    { name: "Jefferson Square", maintainer: "Not maintained" }
+  ],
+  "Crowbank": [
+    { name: "Blight Park", maintainer: "Not maintained" },
+    { name: "Cemetery 73,67", maintainer: "Not maintained" },
+    { name: "Bruton Lane", maintainer: "Not maintained" }
+  ],
   "Dakerstown": [
-    { name: "Junkyard 6,7", maintainer: "TheLast" }
+    { name: "Junkyard 6,7", maintainer: "Not maintained" }
+  ],
+  "Danversbank": [
+    { name: "Cemetery 73,97", maintainer: "Not maintained" },
+    { name: "Angerstein Park", maintainer: "Not maintained" }
+  ],
+  "Dartside": [
+    { name: "The Abarrow Monument", maintainer: "Not maintained" },
+    { name: "Corless Way", maintainer: "Not maintained" },
+    { name: "Crumpler Road", maintainer: "Not maintained" },
+    { name: "Lyng Street", maintainer: "Not maintained" }
   ],
   "Darvall Heights": [
-    { name: "Salopia Row", maintainer: "Soldiers of Crossman" }
+    { name: "Salopia Row", maintainer: "Soldiers of Crossman" },
+    { name: "Cemetery 24,27", maintainer: "Not maintained" },
+    { name: "Nurten Avenue", maintainer: "Soldiers of Crossman" }
+  ],
+  "Dentonside": [
+    { name: "Cemetery 97,57", maintainer: "Not maintained" },
+    { name: "Cowdry Walk", maintainer: "Not maintained" },
+    { name: "Cowley Walk", maintainer: "Not maintained" }
+  ],
+  "Dulston": [
+    { name: "Hamerton Road", maintainer: "Not maintained" },
+    { name: "Duport Avenue", maintainer: "Not maintained" },
+    { name: "Cemetery 99,2", maintainer: "Not maintained" }
+  ],
+  "Dunell Hills": [
+    { name: "Cemetery 8,38", maintainer: "Not maintained" },
+    { name: "Hilborn Walk", maintainer: "Not maintained" }
+  ],
+  "Dunningwood": [
+    { name: "Cemetery 90,20", maintainer: "Not maintained" }
+  ],
+  "Earletown": [
+    { name: "Ames Plaza", maintainer: "Not maintained" }
+  ],
+  "East Becktown": [
+    { name: "Coymer Square", maintainer: "Not maintained" }
+  ],
+  "East Boundwood": [
+    { name: "Ambrose Lane", maintainer: "Not maintained" }
+  ],
+  "East Grayside": [
+    { name: "The Wicklen Monument", maintainer: "Not maintained" },
+    { name: "Gutch Park", maintainer: "Not maintained" },
+    { name: "The Bligh Monument", maintainer: "Not maintained" },
+    { name: "Prigg Alley", maintainer: "Not maintained" },
+    { name: "Baynton Street", maintainer: "Not maintained" },
+    { name: "Mathews Park", maintainer: "Not maintained" }
+  ],
+  "Edgecombe": [
+    { name: "Moon Way", maintainer: "Not maintained" },
+    { name: "Digby Walk", maintainer: "Not maintained" },
+    { name: "Oldidge Way", maintainer: "Not maintained" }
+  ],
+  "Foulkes Village": [
+    { name: "Fyfhyde Plaza", maintainer: "Not maintained" }
+  ],
+  "Fryerbank": [
+    { name: "Bulmer Street", maintainer: "Not maintained" },
+    { name: "Cemetery 92,85", maintainer: "Not maintained" },
+    { name: "Kingham Drive", maintainer: "Not maintained" },
+    { name: "Pickford Park", maintainer: "Not maintained" }
+  ],
+  "Galbraith Hills": [
+    { name: "Clearey Drive", maintainer: "Not maintained" }
+  ],
+  "Gatcombeton": [
+    { name: "Blabey Park", maintainer: "Not maintained" }
+  ],
+  "Greentown": [
+    { name: "North Square", maintainer: "Not maintained" }
+  ],
+  "Grigg Heights": [
+    { name: "Riddles Place", maintainer: "Not maintained" },
+    { name: "Sherring Place", maintainer: "Not maintained" },
+    { name: "Forshaw Drive", maintainer: "Not maintained" }
+  ],
+  "Havercroft": [
+    { name: "Cemetery 31,47", maintainer: "Not maintained" }
+  ],
+  "Heytown": [
+    { name: "Cemetery 72,38", maintainer: "Not maintained" },
+    { name: "Cemetery 74,36", maintainer: "Not maintained" },
+    { name: "Mester Square", maintainer: "Not maintained" }
+  ],
+  "Hollomstown": [
+    { name: "Cemetery 66,92", maintainer: "Not maintained" },
+    { name: "Cemetery 66,97", maintainer: "Not maintained" }
+  ],
+  "Houldenbank": [
+    { name: "Carpark 92,67", maintainer: "Not maintained" },
+    { name: "Ford Drive", maintainer: "Not maintained" },
+    { name: "The Greatorex Building", maintainer: "Not maintained" }
+  ],
+  "Huntley Heights": [
+    { name: "Cemetery 68,25", maintainer: "Not maintained" }
+  ],
+  "Jensentown": [
+    { name: "Cemetery 15,8", maintainer: "Not maintained" },
+    { name: "Wasteland 15,1", maintainer: "Not maintained" }
+  ],
+  "Judgewood": [
+    { name: "Cemetery 11,13", maintainer: "Not maintained" },
+    { name: "Cemetery 16,11", maintainer: "Not maintained" },
+    { name: "Wasteland 15,18", maintainer: "Not maintained" },
+    { name: "Wasteland 16,15", maintainer: "Not maintained" }
+  ],
+  "Kempsterbank": [
+    { name: "Ware Walk", maintainer: "Not maintained" },
+    { name: "Ebbutt Road", maintainer: "Not maintained" },
+    { name: "Cemetery 59,79", maintainer: "Not maintained" }
+  ],
+  "Ketchelbank": [
+    { name: "St. Matthias's Church", maintainer: "Not maintained" }
+  ],
+  "Kinch Heights": [
+    { name: "Cemetery 43,85", maintainer: "Not maintained" },
+    { name: "Cemetery 49,88", maintainer: "Not maintained" }
+  ],
+  "Lamport Hills": [
+    { name: "The McLean Monument", maintainer: "Not maintained" },
+    { name: "Cemetery 53,9", maintainer: "Not maintained" },
+    { name: "Church Square", maintainer: "Not maintained" }
+  ],
+  "Lerwill Heights": [
+    { name: "Carpark 22,53", maintainer: "Not maintained" },
+    { name: "Ebsworth Row", maintainer: "Not maintained" },
+    { name: "The Pollitt Monument", maintainer: "Not maintained" },
+    { name: "Bragg Park", maintainer: "Not maintained" }
+  ],
+  "Lockettside": [
+    { name: "Butler Avenue", maintainer: "Not maintained" }
+  ],
+  "Lukinswood": [
+    { name: "Cemetery 22,46", maintainer: "Not maintained" },
+    { name: "Rayner Crescent", maintainer: "Not maintained" }
+  ],
+  "Miltown": [
+    { name: "The Bizzell Monument", maintainer: "Not maintained" },
+    { name: "Tickle Crescent", maintainer: "Not maintained" },
+    { name: "Chiles Park", maintainer: "Not maintained" }
+  ],
+  "Mockridge Heights": [
+    { name: "Cemetery 46,63", maintainer: "Not maintained" }
+  ],
+  "Molebank": [
+    { name: "Wadden Boulevard", maintainer: "Not maintained" },
+    { name: "Cemetery 14,42", maintainer: "Not maintained" },
+    { name: "Hanlon Park", maintainer: "Not maintained" }
+  ],
+  "Mornington": [
+    { name: "Sheridan Street", maintainer: "Not maintained" },
+    { name: "Cemetery 17,67", maintainer: "Not maintained" }
+  ],
+  "New Arkham": [
+    { name: "Methringham Grove", maintainer: "Not maintained" },
+    { name: "Redpath Place", maintainer: "Not maintained" }
+  ],
+  "North Blythville": [
+    { name: "Male Boulevard", maintainer: "Not maintained" },
+    { name: "Cemetery 27,63", maintainer: "Not maintained" },
+    { name: "Shallet Crescent", maintainer: "Not maintained" }
+  ],
+  "Old Arkham": [
+    { name: "Cemetery 15,97", maintainer: "Not maintained" }
+  ],
+  "Osmondville": [
+    { name: "Barrington Walk", maintainer: "Not maintained" }
+  ],
+  "Owsleybank": [
+    { name: "McNamara Drive", maintainer: "Not maintained" },
+    { name: "The Woodborne Building", maintainer: "Not maintained" },
+    { name: "The Sendall Monument", maintainer: "Not maintained" }
+  ],
+  "Pegton": [
+    { name: "Cemetery 87,57", maintainer: "Not maintained" }
+  ],
+  "Pennville": [
+    { name: "Wasteland 82,82", maintainer: "Not maintained" },
+    { name: "Cemetery 85,86", maintainer: "Not maintained" }
+  ],
+  "Penny Heights": [
+    { name: "War Crescent", maintainer: "Not maintained" },
+    { name: "Junkyard 98,76", maintainer: "Not maintained" }
+  ],
+  "Pitneybank": [
+    { name: "Cemetery 81,45", maintainer: "Not maintained" }
+  ],
+  "Reganbank": [
+    { name: "Cemetery 11,56", maintainer: "Not maintained" },
+    { name: "Cemetery 13,58", maintainer: "Not maintained" },
+    { name: "Cemetery 16,56", maintainer: "Not maintained" }
+  ],
+  "Rhodenbank": [
+    { name: "The Upshall Monument", maintainer: "Not maintained" },
+    { name: "Orome Avenue", maintainer: "Not maintained" }
+  ],
+  "Richmond Hills": [
+    { name: "Shehan Boulevard", maintainer: "Not maintained" }
+  ],
+  "Ridleybank": [
+    { name: "Margery Avenue", maintainer: "Not maintained" }
+  ],
+  "Roachtown": [
+    { name: "Trotter Place", maintainer: "Not maintained" }
+  ],
+  "Roftwood": [
+    { name: "Turpin Road", maintainer: "Not maintained" },
+    { name: "Swansborough Park", maintainer: "Not maintained" }
+  ],
+  "Ruddlebank": [
+    { name: "Peppe Park", maintainer: "Not maintained" },
+    { name: "Greenleaves Alley", maintainer: "Not maintained" }
+  ],
+  "Santlerville": [
+    { name: "Cookesley Avenue", maintainer: "Not maintained" },
+    { name: "St. Emelia's Church", maintainer: "Not maintained" },
+    { name: "Junkyard 74,26", maintainer: "Not maintained" },
+    { name: "Cemetery 78,26", maintainer: "Not maintained" },
+    { name: "The Dewes Building", maintainer: "Not maintained" },
+    { name: "Wasteland 78,21", maintainer: "Not maintained" }
+  ],
+  "Shackleville": [
+    { name: "The Stanbury Monument", maintainer: "Not maintained" },
+    { name: "Copp Avenue", maintainer: "Not maintained" },
+    { name: "Swaffield Plaza", maintainer: "Not maintained" },
+    { name: "Cemetery 57,69", maintainer: "Not maintained" }
+  ],
+  "Shuttlebank": [
+    { name: "Look Road", maintainer: "Not maintained" }
+  ],
+  "South Blythville": [
+    { name: "Gee Avenue", maintainer: "Not maintained" },
+    { name: "Park Walk", maintainer: "Not maintained" }
+  ],
+  "Spracklingbank": [
+    { name: "Cemetery 80,34", maintainer: "Not maintained" }
+  ],
+  "Stanbury Village": [
+    { name: "Axworthy Square", maintainer: "Not maintained" }
+  ],
+  "Tapton": [
+    { name: "Dix Place", maintainer: "Not maintained" },
+    { name: "Rhodes Park", maintainer: "Not maintained" }
+  ],
+  "Vinetown": [
+    { name: "The Bhore Monument", maintainer: "Not maintained" },
+    { name: "The Dinovan Monument", maintainer: "Not maintained" }
+  ],
+  "West Boundwood": [
+    { name: "Cemetery 38,7", maintainer: "Not maintained" }
+  ],
+  "West Grayside": [
+    { name: "Bunstone Alley", maintainer: "Not maintained" },
+    { name: "Cemetery 52,83", maintainer: "Not maintained" }
+  ],
+  "Williamsville": [
+    { name: "Cemetery 34,97", maintainer: "Not maintained" },
+    { name: "Carpark 36,94", maintainer: "Not maintained" },
+    { name: "Waller Crescent", maintainer: "Not maintained" }
+  ],
+  "Wray Heights": [
+    { name: "Flynn Alley", maintainer: "Not maintained" },
+    { name: "Last Road", maintainer: "Not maintained" }
+  ],
+  "Wyke Hills": [
+    { name: "Craddock Road", maintainer: "Not maintained" },
+    { name: "Cemetery 54,97", maintainer: "Not maintained" },
+    { name: "Knight Square", maintainer: "Not maintained" },
+    { name: "The Wicksted Building", maintainer: "Not maintained" },
+    { name: "Cemetery 51,99", maintainer: "Not maintained" }
+  ],
+  "Yagoton": [
+    { name: "St. Swithun's Church", maintainer: "Not maintained" }
   ]
 };
