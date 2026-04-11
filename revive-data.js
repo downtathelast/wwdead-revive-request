0a1195860eaf51a9bd0b16fac056880b0401de49
DTTL <69401746+downtathelast@users.noreply.github.com>
2026-04-11 14:53:20 -0700

    Add REVIVE_DATA object with locations and maintainers

---
+++ revive-data.js
@@ -0,0 +1,8 @@
+const REVIVE_DATA = {
+  "Dakerstown": [
+    { name: "Junkyard 6,7", maintainer: "TheLast" }
+  ],
+  "Darvall Heights": [
+    { name: "Salopia Row", maintainer: "Soldiers of Crossman" }
+  ]
+};
