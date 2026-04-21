20a02c6b125dfdde526c8732179fa05f9e0b571d
DTTL <69401746+downtathelast@users.noreply.github.com>
2026-04-20 19:25:13 -0700

    Fix Central suburbs list in script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ document.addEventListener("REVIVE_DATA_READY", function () {
     NE: ["Lamport Hills","Chancelwood","Earletown","Rhodenbank","Dulston","Millen Hills","Raines Hills","Pashenton","Rolt Heights","Pescodside","Shearbank","Huntley Heights","Santlerville","Gibsonton","Dunningwood","Heytown","Spracklingbank","Paynterton","Peppardville","Pitneybank","Starlingtown"],
     SW: ["Grigg Heights","Reganbank","Lerwill Heights","Crooketown","Mornington","North Blythville","Nixbank","Wykewood","South Blythville","Greentown","Tapton","Foulkes Village","Ruddlebank","Lockettside","Dartside","Kinch Heights","New Arkham","Old Arkham","Spicer Hills","Williamsville","Buttonville"],
     SE: ["Edgecombe","Pegton","Dentonside","Crowbank","Vinetown","Houldenbank","Kempsterbank","Wray Heights","Gulsonside","Osmondville","Penny Heights","West Grayside","East Grayside","Scarletwood","Pennville","Fryerbank","Wyke Hills","Hollomstown","Danversbank","Whittenside","Miltown"],
-    Central: ["Richmond Hills","Ketchelbank","Roachtown","Randallbank","Havercroft","Barrville","Ridleybank","Pimbank","Shore Hills","Galbraith Hills","Stanbury Village","Roftwood","Brooksville","Mockridge Heights","Shackleville","Tollyton"],
+    Central: ["Richmond Hills","Ketchelbank","Roachtown","Randallbank","Havercroft","Barrville","Ridleybank","Pimbank","Shore Hills","Galbraith Hills","Stanbury Village","Roftwood","Brooksville","Mockridge Heights","Shackleville","Tollyton"]
   };
   // -----------------------------
   // SECTOR → SUBURB
